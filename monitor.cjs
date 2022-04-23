3171538596c8396189ba12a0acc63a54e1863698
ceribe <ceribekagami@gmail.com>
2022-04-23 18:53:42 +0200

    Add inital files

---
+++ monitor.cjs
@@ -0,0 +1,13 @@
+"use strict";
+
+class Monitor {
+  constructor() {}
+
+  wait() {}
+
+  notify() {}
+
+  notifyAll() {}
+}
+
+module.exports = { Monitor };
---
+++ test.js
@@ -0,0 +1,6 @@
+"use strict";
+import { Monitor } from "./monitor.cjs";
+
+const monitor = new Monitor();
+
+console.log(process.argv.slice(2));
