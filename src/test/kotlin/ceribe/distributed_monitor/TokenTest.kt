b76c11caad1c04fe6accf945bd80be0803aec6fb
ceribe <ceribekagami@gmail.com>
2022-05-11 16:35:49 +0200

    Add test module and a dummy test

---
+++ src/test/kotlin/ceribe/distributed_monitor/TokenTest.kt
@@ -0,0 +1,10 @@
+package ceribe.distributed_monitor
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class TokenTest {
+    @org.junit.jupiter.api.Test
+    fun `should create token`() {
+        assertEquals(1, 1)
+    }
+}
